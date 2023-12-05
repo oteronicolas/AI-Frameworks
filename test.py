5e9bb988b4214531cf746dc28ac2d810e6b961f8
Noteroinsa <notero@etud.insa-toulouse.fr>
2023-12-05 04:56:17 +0100

    recommandations pour n'importe quelle image

--- test.py
+++ test.py
@@ -1,13 +1,58 @@
 from methods import create_df
 from multiprocessing import freeze_support
+from torchvision import datasets
 
+
+class ImageAndPathsDataset(datasets.ImageFolder):
+    def __getitem__(self, index):
+        img, _ = super(ImageAndPathsDataset, self).__getitem__(index)
+        path = self.imgs[index][0]
+        return img, path
 def main():
-    freeze_support()  # need this line for Windows multiprocessing
-    dico = {'df': create_df('./extract_data/MLP-20M'),
-            'n_reco': 5,
-            'index': 16}
-    print(dico['df'].head())
-    print(dico)
+    import torch
+    from torch.utils.data import DataLoader
+    import torchvision.transforms as transforms
+
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    normalize = transforms.Normalize(mean, std)
+    inv_normalize = transforms.Normalize(
+        mean=[-m / s for m, s in zip(mean, std)],
+        std=[1 / s for s in std]
+    )
+
+    transform = transforms.Compose([transforms.Resize((224, 224)),
+                                    transforms.ToTensor(),
+                                    normalize])
+    dataset = ImageAndPathsDataset('./extracted_data//movielens-20m-posters-for-machine-learning/MLP-20M', transform)
+
+    dataloader = DataLoader(dataset, batch_size=128, num_workers=2, shuffle=False)
+    import torchvision.models as models
+    mobilenet = models.mobilenet_v3_small(weights='DEFAULT')
+    import torch.nn as nn
+    mobilenet = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights)
+    model = mobilenet
+    model.classifier = nn.Flatten()
+    import pandas as pd
+    from tqdm.notebook import tqdm
+
+    features_list = []
+    paths_list = []
+
+    for x, paths in tqdm(dataloader):
+        with torch.no_grad():
+            embeddings = model(x)
+            features_list.extend(embeddings.numpy())
+            paths_list.extend(paths)
+
+    df = pd.DataFrame({
+        'features': features_list,
+        'path': paths_list
+    })
+    import os
+    current_dir = os.getcwd()
+    pickle_file_path = 'df.pkl'
+    df.to_pickle(f'{current_dir}+{pickle_file_path}')
 
 if __name__ == '__main__':
     main()
--- webapp.py
+++ webapp.py
@@ -1,35 +1,29 @@
 import gradio as gr
 from PIL import Image
 from methods import create_df
+from methods import extract_features_from_image
 import requests
 import io
 import json
+import pandas
 
 def similar_poster(image):
-    # Conversion de l'image en données binaires
-    #img_binary = io.BytesIO()
-    #image.save(img_binary, format="PNG")
-
-    #api_url = "http://df63-104-196-8-77.ngrok.io/upload"
-
-    # Utilisez le nom de fichier "image" pour envoyer l'image à l'API Flask
-    #response = requests.post(api_url, data=img_binary.getvalue())
-    #print(response)
-    df = create_df('./extract_data/MLP-20M')
-    index_movie = 1
-    dico = {'nb_reco': 5,
-            'vec': df['features'].iloc[index_movie].tolist()}
-
+    #df = create_df('./extracted_data//movielens-20m-posters-for-machine-learning/MLP-20M')
+    df = pandas.read_pickle("df.pkl")
+    #index_movie = 15
+    image_features = extract_features_from_image(image)
+    #dico = {'nb_reco': 5,
+    #        'vec': df['features'].iloc[index_movie].tolist()}
+    dico = {'nb_reco': 5, 'vec': image_features.tolist()}
     jsonData = json.dumps(dico)
     headers = {'Content-Type': 'application/json'}
 
-    response = requests.post("http://10.32.47.159:5000/predict", data=jsonData, headers=headers)
+    response = requests.post("http://127.0.0.1:5000/predict", data=jsonData, headers=headers)
     pred = df.iloc[response.json()["prediction"]]
-
-
+    paths=pred.path
+    print(paths)
     if response.status_code == 200:
-        similar_image_paths = response.json()["similar_poster"]
-        similar_images = [Image.open(path) for path in similar_image_paths]
+        similar_images = [Image.open(path) for path in paths]
         all_images =  similar_images
 
         return all_images[0], all_images[1], all_images[2], all_images[3],all_images[4]
@@ -39,7 +33,7 @@ if __name__ == '__main__':
     gr.Interface(
         fn=similar_poster,
         inputs=gr.Image(type="pil", label="Choose an image file"),
-        outputs=["image","image","image"],
+        outputs=["image","image","image","image","image"],
         live=True,
         description="Select an image file and view it along with 5 similar images.",
     ).launch(debug=True, share=True)
